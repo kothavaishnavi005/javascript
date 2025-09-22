82212c6c70f0e55f9ef53cb226292f4fbed0b3e0
vaishnavi <kothavaishnavi005@gmail>
2025-09-22 14:15:24 +0530

    update changes

--- datatypes.js
+++ datatypes.js
@@ -1,4 +1,21 @@
-a= 7897
-console.log(typeof a);
-name = "john";
-console.log(typeof name);
+//number
+let age=20;
+//string
+name="tkr";
+//boolean
+true or false
+//undefined
+let a ;
+console.log(x);
+//null
+let y=null;
+//symbol
+let id=Symbol("uniqueID")
+// non primitive
+//object
+let a={name:"john",age:20};
+//array
+let number={1,2,3}
+//function
+function greet(){return "haii";}
+
---
+++ function.js
@@ -0,0 +1,19 @@
+function add()
+{
+const add =() =>{
+    var a=20;
+    var b=16;
+    document.write(a+b);
+    console.log(a+b);
+
+}
+add();
+//EST
+const add = (a,b) => console.log(a + b);
+add(16, 20)
+}
+let name = "john";
+
+function demo()
+let  name ="shivaa"
+document.write("hello 
